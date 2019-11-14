1d6cd527d359b85dd4d4c0460adbe71c1c592b63
arearius <arearius@gmail.com>
2019-11-14 10:12:16 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,10 +5,13 @@ ini_set('log_errors', 1);
 session_start();
 include_once __DIR__ . '/autoload.php';
 
+print_($_GET);
+print_($_POST);
+
+
 $action = $_GET['action'] ?? 'TasksList';
 switch ($_GET['controller']) {
     case 'AuthController' : $controller = new AuthController(); echo 'auth';
     default : $controller = new TasksController(); 
 }
-
 $controller->{$action}();

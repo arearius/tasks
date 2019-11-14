a41fc4d640f011c11fcf35a7d5ac3971340442b1
arearius <arearius@gmail.com>
2019-11-14 10:49:26 +0300

    Update AuthController.php

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -16,6 +16,11 @@ class AuthController
 
     public function signOut(){
         Auth::signOut();
+        if (isset($_GET['page'])) $page=$_GET['page'];
+    	else $page = 0;
+        if (isset($_GET['sortBy'])) $sort=$_GET['sortBy'];
+        $tasks = $this->task->getTasks($page, $sort);
+	    $this->view->show('TasksList', $tasks);
         return true;
     }
 

0d1ceb832b81b8b242f74b5a6872362c45df9c0a
arearius <arearius@gmail.com>
2019-11-14 09:00:21 +0300

    Update AuthController.php

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -4,4 +4,17 @@
 class AuthController
 {
 
+    public function signUp(){
+        if (Auth::signUp()){
+            return 'ok';
+        } else {
+            return 'Wrong password';
+        }
+    }
+
+    public function signOut(){
+        Auth::signOut();
+        return true;
+    }
+
 }
